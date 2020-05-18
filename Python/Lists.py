593c40382c34d88b7369c7545962fe0ef186fe57
Manohar Singh <manohar.aswal@gmail.com>
2020-05-18 23:01:33 +0530

    Modified files.

--- Python/Lists.py
+++ Python/Lists.py
@@ -1,15 +1,19 @@
 if __name__ == '__main__':
-    N = int(raw_input())
-    list = []
-    list.insert(0,5)
-    list.insert(1,10)
-    list.insert(0,6)
-    print list
-    list.remove(6)
-    list.append(9)
-    list.append(1)
-    list.sort()
-    print list
-    list.pop()
-    list.reverse()
-    print list
+    N = int(input())
+    List = []
+    for i in range(N):
+        QueryL = list(map(str, input().strip().split()))
+        if "insert" == QueryL[0]:
+            List.insert(int(QueryL[1]), int(QueryL[2]))
+        elif "print" == QueryL[0]:
+            print(List)
+        elif "remove" == QueryL[0]:
+            List.remove(int(QueryL[1]))
+        elif "append" == QueryL[0]:
+            List.append(int(QueryL[1]))
+        elif "sort" == QueryL[0]:
+            List.sort()
+        elif "pop" == QueryL[0]:
+            List.pop()
+        elif "reverse" == QueryL[0]:
+            List.reverse()
